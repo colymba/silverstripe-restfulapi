92e03ec306f04df48b3f833abd6fbf197eca8ca4
colymba <thierry@colymba.com>
2013-10-26 17:37:49 +0300

    Update API doc

--- doc/elementlist.js
+++ doc/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Inflector"],["c","RESTfulAPI"],["c","RESTfulAPI_Authenticator"],["c","RESTfulAPI_DefaultQueryHandler"],["c","RESTfulAPI_DefaultSerializer"],["c","RESTfulAPI_QueryHandler"],["c","RESTfulAPI_Serializer"],["c","RESTfulAPI_TokenAuthenticator"],["c","stdClass"]];
+ApiGen.elements = [["c","Inflector"],["c","RESTfulAPI"],["c","RESTfulAPI_Authenticator"],["c","RESTfulAPI_DefaultQueryHandler"],["c","RESTfulAPI_DefaultSerializer"],["c","RESTfulAPI_QueryHandler"],["c","RESTfulAPI_Serializer"],["c","RESTfulAPI_TokenAuthenticator"],["c","RESTfulAPI_TokenAuthExtension"],["c","stdClass"]];
