e5b87a727932cf88c546e8c0a3e60dafb70dfc0d
Roman Schmid <bummzack@gmail.com>
2015-05-28 15:26:31 +0200

    FIX #39: Check for token uniqueness when generating tokens. ADD #44: Implement API method to refresh token.
    - Reverted to non-unique indexes on `RESTfulAPI_TokenAuthExtension` since there's an issue with MsSQL DBs.
    - Implemented token refresh methods.
    - Updated documentation.
    - Added test for "refreshToken".
    - Updated token uniqueness test.

--- tests/authenticator/RESTfulAPI_TokenAuthenticator_Test.php
+++ tests/authenticator/RESTfulAPI_TokenAuthenticator_Test.php
@@ -26,6 +26,16 @@ class RESTfulAPI_TokenAuthenticator_Test extends RESTfulAPI_Tester
     return $auth;
   }
 
+  protected function getFlawedAuthenticator()
+  {
+    $injector = new Injector();
+    $auth     = new FlawedAuthenticator();
+
+    $injector->inject($auth);
+
+    return $auth;
+  }
+
 
   public function setUpOnce()
   {
@@ -208,5 +218,109 @@ class RESTfulAPI_TokenAuthenticator_Test extends RESTfulAPI_Tester
       array($result),
       "TokenAuth authentication failure should return a RESTfulAPI_Error"
     );
+  }
+
+  public function testTokenRefresh()
+  {
+    $auth = $this->getAuthenticator();
+    $request = new SS_HTTPRequest(
+      'GET',
+      'api/auth/login',
+      array(),
+      array('email' => 'test@test.com', 'pwd' => 'test')
+    );
+
+    $result = $auth->login($request);
+    $this->assertContains(
+      'refreshtoken',
+      array_keys($result),
+      'Login response should return a refresh token'
+    );
+
+    $token = $result['token'];
+    $refreshtoken = $result['refreshtoken'];
+    $request = new SS_HTTPRequest(
+      'GET',
+      'api/auth/refreshToken',
+      array(),
+      array('refreshtoken' => $refreshtoken)
+    );
+    $request->addHeader('X-Silverstripe-Apitoken', $token);
+
+    $result2 = $auth->refreshToken($request);
+    $resultKeys = array_keys($result2);
+    // test if the result contains values for 'refreshtoken', 'token', 'expire'
+    foreach(array('refreshtoken', 'token', 'expire') as $key){
+      $this->assertContains(
+        $key,
+        $resultKeys,
+        'Refreshing the token should return an array that contains ' . $key
+      );
+    }
+
+    $newToken = $result2['token'];
+    $newRefreshToken = $result2['refreshtoken'];
+    $this->assertThat(
+      $token,
+      $this->logicalNot(
+        $this->equalTo($newToken)
+      ),
+      "TokenAuth refreshToken should generate a new token"
+    );
+
+    $this->assertThat(
+      $refreshtoken,
+      $this->logicalNot(
+        $this->equalTo($newRefreshToken)
+      ),
+      "TokenAuth refreshToken should generate a new refresh token"
+    );
+  }
+
+  /**
+   * Test edge case of a member with the same API token
+   */
+  public function testTokenUniqueness()
+  {
+    $member = Member::get()->filter(array(
+      'Email' => 'test@test.com'
+    ))->first();
+
+    // get an authenticator that always creates the same token
+    $auth = $this->getFlawedAuthenticator();
+    $auth->resetToken($member->ID);
+
+    $newMemberID = Member::create(array(
+      'Email' => 'TestMember',
+      'Password' => 'test'
+    ))->write();
+
+    $hasErrors = false;
+    try {
+      $auth->resetToken($newMemberID);
+    } catch(Exception $e){
+      $hasErrors = true;
+    }
+
+    $this->assertTrue(
+      $hasErrors,
+      'Creating the same token twice should raise an error'
+    );
+  }
+
+
+  public function test()
+  {
+  }
+}
+
+/**
+ * Flawed authenticator to test the case when non-unique tokens are generated
+ */
+class FlawedAuthenticator extends RESTfulAPI_TokenAuthenticator
+{
+  protected function generateToken()
+  {
+    return 'I always return the same token!';
   }
 }
